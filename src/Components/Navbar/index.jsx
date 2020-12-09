9ffd6f36aa534e3f838583e6a3ae168ca8cea3fa
Damien <damienbanville@hotmail.fr>
2020-12-09 12:17:32 +0100

    Sidebar and navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,27 @@
+import {Link} from "react-router-dom";
+import React from 'react'
+import { useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+
+
+const Nav = () => {
+
+    return (
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <a className="navbar-brand" href="#">DIPPR</a>
+        <div className="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to="/" className="navbar-brand">Home</Link>
+            <Link to="#" className="nav-item nav-link">About</Link>
+          </div>
+          <div className="Connection">
+            <Button href="#" variant="outline-primary">Se connecter</Button>{' '}
+            <Button href="#" variant="primary">S'inscrire</Button> 
+          </div>
+        </div>
+      </nav>
+    );
+  };
+
+export default Nav;
