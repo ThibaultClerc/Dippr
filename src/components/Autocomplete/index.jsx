342d290ec51ff998aae7043d618d259d5e956866
bakbledy93 <phim.kevin@gmail.com>
2020-12-10 15:45:18 +0100

    tag and ingredients autocomplete

--- src/components/Autocomplete/index.jsx
+++ src/components/Autocomplete/index.jsx
@@ -3,85 +3,34 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
 import CloseIcon from '@material-ui/icons/Close';
-import { Badge } from "react-bootstrap";
+import { Badge, Table } from "react-bootstrap";
 
 
-const AutocompleteSearchBar = ({value}) =>{
-  let array = [];
-  const [ingredients, setIngredients] = useState(value);
-  const [ingredient, setIngredient] = useState("");
-  const [disheIngredients, setDisheIngredients] = useState(array);
+const AutocompleteSearchBar = ({content, title}) =>{
+  const [currentTitle,setCurrentTitle] = useState(title);
+  const [currentValues, setCurrentValues] = useState(content);
+  const [data, setData] = useState([]);
 
-  const addElement = () => {
-    console.log(ingredient)
-    let ingredient_id;
-    ingredients.map(element =>{
-      if(element.type === ingredient) {
-        return ingredient_id = element.id
-        }
-      }
-    );
-    setDisheIngredients(array =>[...array,ingredient_id]);
+  const addElement = (event, values) => {
+    console.log(values)
+    setData(values)
   };
-
-  const removeElement = (element) => {
-    let array2 = disheIngredients;
-
-    const index = array2.indexOf(element);
-    console.log(index)
-    if(index > -1){
-      array2.splice(index, 1)
-    }
-    document.getElementById(element).innerHTML=""
-    setDisheIngredients(array2);
-    };
-
-  const ingredientBadges = () => {
-    let table = []
-    console.log("hello")
-    disheIngredients.forEach(element =>{ 
-      ingredients.map(value =>{
-        if(value.id == element) {
-            return table.push(
-            <>
-            <Badge variant="secondary" id={element}>{value.type}<CloseIcon onClick={()=>removeElement(element)} style={{cursor: "pointer"}}  id={element + "a"}/></Badge>
-            </>
-            )
-          }
-        }
-      );
-      
-    })
-    return table
-
-  };
-
-  useEffect(() => {
-    console.log(value)
-  }, []);
-
-  useEffect(() => {
-    console.log(disheIngredients)
-  }, [disheIngredients]);
-
-  useEffect(() => {
-    return () => {
-      removeElement()
-    }
-  }, []);
 
   return(
     <>
+    
     <Autocomplete
+    multiple
     id="combo-box-demo"
-    options={ingredients}
+    options={currentValues}
     getOptionLabel={(options) => options.type}
-    style={{ width: 300 }}
-    renderInput={(params) => <TextField {...params} label="Ingredients" variant="outlined" onSelect={e => setIngredient(e.target.value)}/>}
-  /><Button variant="contained" onClick={addElement} >Ajouter</Button> <br></br>{ingredientBadges()}
+    style={{ width: 600 }}
+    onChange={addElement}
+    renderInput={(params) => <TextField {...params} label={currentTitle} variant="outlined"/>}
+    />
+
 
   </>
-
   )
 };
 
--- src/pages/Annoucement/index.jsx
+++ src/pages/Annoucement/index.jsx
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react'
-
-import AutocompleteSearchBar from '../../components/Autocomplete'
+import {Container, Row, Col, Form, Button } from "react-bootstrap";
+import SearchBar from '../../components/Autocomplete'
 
 const Announcement = () => {
   const [ingredients, setIngredients] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+
+  const data = {
+    user_dishe: {
+      user_id: id,
+      name: name,
+      description: description,
+      date: date,
+    }
+};
+
 
   const fetchIngredient = () => {
     fetch("https://dippr-api-development.herokuapp.com/api/ingredients", {
@@ -28,18 +43,46 @@ const Announcement = () => {
     })
   };
 
+  const fetchTag = () => {
+    fetch("https://dippr-api-development.herokuapp.com/api/tags", {
+      "method": "GET",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      const tag = response.data;
+      const array_tags = []
+        tag.forEach(element =>{
+          array_tags.push(element);
+          }
+        )
+      setTags(array_tags)
+    }).catch(error => {
+      console.log(error)
+    })
+  };
+
   useEffect(() => {
     fetchIngredient();
+    fetchTag();
   }, []);
 
   useEffect(() => {
     console.log(ingredients)
+    console.log(tags)
   }, [ingredients]);
 
 
   return(
     <>
-    {ingredients.length !== 0 && <AutocompleteSearchBar value={ ingredients }/>}
+    {ingredients.length !== 0 && <SearchBar content={ ingredients } title="Ingredients"/>}
+    <br/>
+    {tags.length !== 0 && <SearchBar content={ tags } title="Type de plat"/>}
+
     </>
   )
 }
