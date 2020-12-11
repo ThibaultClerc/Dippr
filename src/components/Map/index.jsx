159ee177b5ccfe5726c0f46b17e634cf1be7252a
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-11 19:17:45 +0100

    Can click on dish on map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import Loader from '../UI/Loader'
 import {
   GoogleMap,
@@ -6,9 +6,8 @@ import {
   Marker,
   InfoWindow
 } from '@react-google-maps/api';
-import Button from 'react-bootstrap/Button';
-import '../../assets/img/compass.png'
-
+import dishLogo from '../../assets/img/dishLogo.png';
+import DishCard from '../DishCard'
 
 const libraries = ["places"]
 
@@ -18,53 +17,28 @@ const mapContainerStyle = {
 }
 
 const center = {
-  lat: 48.856613,
-  lng: 2.352222
+  lat: 48.813533,
+  lng: 2.343833
 }
 
 const options = {
   disableDefaultUI: true
 }
 
-
-
-
-
-
-
-
 const Map = ({data}) => {
+  const [selected, setSelected] = useState(null);
   const {isLoaded, loadError} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   })
 
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, [])
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return <Loader/>
-
-  // const panTo = useCallback(({lat, lng}) => {
-
-  // }, [])
-
-  // const Locate = ({panTo}) => {
-  //   return (
-  //     <Button className="locate">
-  //       <img src="../../assets/img/compass.png" alt="compass - locate me"></img>
-  //     </Button>
-  //   ) 
-  // }
-
-
-
-  // useEffect(() => {
-  //   setMarkers()
-  // }, [data])
-
-  // const setMarkers = () => {
-    
-  // }
-
-  console.log(data)
 
   return (
     <>
@@ -73,10 +47,35 @@ const Map = ({data}) => {
       zoom={13}
       center={center}
       options={options}
+      onLoad={onMapLoad}
       >
       {data.map(dish => {
-        return <Marker key={dish.attributes.id} position={{lat: parseFloat(dish.meta.user_lat), lng: parseFloat(dish.meta.user_lng)}}/>
+        return <Marker
+                key={dish.attributes.id}
+                position={{lat: parseFloat(dish.meta.user_lat), lng: parseFloat(dish.meta.user_lng)}}
+                icon={{
+                  url: dishLogo,
+                  scaledSize: new window.google.maps.Size(30, 30),
+                  origin: new window.google.maps.Point(0, 0),
+                  anchor: new  window.google.maps.Point(15, 15)
+                }}
+                onClick={() => {
+                  setSelected(dish);
+                }}
+                />
       })}
+      {selected ? (<InfoWindow position={{lat: parseFloat(selected.meta.user_lat), lng: parseFloat(selected.meta.user_lng)}}>
+        <DishCard
+          key={selected.id}
+          market_dish_id={selected.id}
+          name={selected.meta.user_dish.name}
+          description={selected.meta.user_dish.description}
+          dish_rating={selected.meta.user_dish.dish_rating}
+          user_id={selected.meta.user_dish.user_id}
+          created_at={selected.meta.user_dish.created_at}
+          type={selected.attributes.market_dish_type}
+        />
+      </InfoWindow>) : null}
     </GoogleMap>
     
     </>
