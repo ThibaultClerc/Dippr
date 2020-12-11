079eec77fd7ccf25d12e0d8c29ec424e61c26fd9
bakbledy93 <phim.kevin@gmail.com>
2020-12-11 16:31:22 +0100

    add Bottom Bar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from "../Navbar";
 import Home from '../../pages/Home'
@@ -8,11 +8,20 @@ import Annoucement from "../../pages/Annoucement"
 import Profile from "../../pages/Profile"
 import PrivateRoute from "../PrivateRoute"
 import Search from "../../pages/Search"
+import BottomAppBar from "../../components/AppBar"
+import useDeviceDetect from "../DeviceDetect"
 
 const Layout = () => {
+  const { isMobile } = useDeviceDetect();
+
+  useEffect(() => {
+  }, [isMobile])
+
   return (
     <Router>
-      <Nav/>
+      {!isMobile && <Nav/>}
+      {isMobile && <BottomAppBar/>}
+
       <Switch> 
         <Route exact path="/announcement" component={Annoucement}/>
         <Route        exact path="/"        component={Home}   />
