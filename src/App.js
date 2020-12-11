27d83712bcd3d570b7c5fb2ec1b0366a5affc447
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-11 11:29:06 +0100

    Added @react-google-maps/api and imports in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import {
+  GoogleMap,
+  UseLoadScript,
+  Marker,
+  InfoWindow
+} from 'google-maps-react'
 import Layout from './components/Layout';
 
 const App = () => {
