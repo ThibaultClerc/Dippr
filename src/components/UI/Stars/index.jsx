c59b77c8b7a6b2ffd1452af84cca813e022187f2
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-14 22:24:41 +0100

    cards style ok

--- src/components/UI/Stars/index.jsx
+++ src/components/UI/Stars/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import StarIcon from '@material-ui/icons/Star';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  star: {
+    fontSize: "1rem"
+  }
+})
+);
 
 const Stars = ({dish_rating}) => {
+  const classes = useStyles();
 
   const starsRender = (dish_rating) => {
     const stars = []
     for(let i=1; i <= dish_rating ; i++) {
-      stars.push(<StarIcon/>)
+      stars.push(<StarIcon className={classes.star}/>)
     }
     return stars
   }
 
   return (
-    <div className="stars">
+    <div>
       {starsRender(dish_rating)}
     </div>
   )
