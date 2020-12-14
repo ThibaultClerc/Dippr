adb9efcfe54fe8cb6af43e0172bbec0ea3c40069
bakbledy93 <phim.kevin@gmail.com>
2020-12-14 09:47:00 +0100

    PWA setting

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -18,6 +18,10 @@ const isLocalhost = Boolean(
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
+const swUrl = `${process.env.PUBLIC_URL}/sw.js` 
+
+
+
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
