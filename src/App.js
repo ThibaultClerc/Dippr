25bdc64efcc522d55ca97321b26a6889e3f72fe2
bakbledy93 <phim.kevin@gmail.com>
2020-12-14 14:40:03 +0100

    add Modal for Safari User

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import Layout from './components/Layout';
-
-
+import  useIsIOS  from './components/IosDetect';
+import { InstallPWA } from './components/InstallPwa';
 
 const App = () => {
+  const { prompt } = useIsIOS();
 
   return (
     <Provider store={store}>
+      {prompt && <InstallPWA />}
       <Layout/>
     </Provider>
   )
